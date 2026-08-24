acf612e4c915147135b4ef2cfa4c4d1ea799961b
Matthew Love <33066727+matth-love@users.noreply.github.com>
2026-08-24 10:47:10 -0700

    vsicurl head, htdp coarse grids (#118)
    * cap blur_sigma at 50
    
    * revert modules.base to No header call; update htdp to use max nx/ny

--- src/transformez/modules/base.py
+++ src/transformez/modules/base.py
@@ -83,11 +83,11 @@ class TransformezModule(FetchModule):
 
             env_kwargs = {
                 "GDAL_HTTP_USERAGENT": DEFAULT_USER_AGENT,
-                "CPL_VSIL_CURL_USE_HEAD": "YES",
+                "CPL_VSIL_CURL_USE_HEAD": "NO",
                 "GDAL_DISABLE_READDIR_ON_OPEN": "YES",
                 "HDF5_USE_FILE_LOCKING": "FALSE",
                 "VSI_CACHE": "TRUE",
-                "VSI_CACHE_SIZE": "50000000",
+                "VSI_CACHE_SIZE": "50000000",  # 50MB cache to stop RAM trashing
             }
 
             with rasterio.Env(**env_kwargs):
