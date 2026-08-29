d49c128bbc16da2b5514e3c2362e64cad2cf0fe8
Matthew Love <matthew.love@colorado.edu>
2026-08-28 20:33:25 -0700

    update logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,11 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os
 import sys
+from pathlib import Path
 
-sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath("../_ext"))
+sys.path.insert(0, str(Path(__file__).resolve().parents[2] / "src"))
 
 project = "Transformez"
 copyright = "2026, The Continuous-DEMs Development Team."
